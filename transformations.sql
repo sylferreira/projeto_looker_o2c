6557fb69b7ee53192960e2e037621e3156196ea3
Sylvia Ferreira <64382706+sylferreira@users.noreply.github.com>
2025-12-03 16:30:03 -0300

    Update comments and metrics in transformations.sql

--- transformations.sql
+++ transformations.sql
@@ -1,25 +1,25 @@
--- VIEW PRINCIPAL PARA O LOOKER
+--Looker view
 CREATE OR REPLACE VIEW `sap-orders-2324.sap_template_2023_2024.vw_o2c_metrics` AS
 SELECT
     *,
     
-    -- MÉTRICAS
+    -- metrics
     SAFE_DIVIDE(delivery_qty, order_qty) AS fill_rate,
     SAFE_DIVIDE(confirmed_qty, order_qty) AS confirmation_rate,
     SAFE_DIVIDE(rejected_qty, order_qty) AS rejection_rate,
     
-    -- LEAD TIMES
+    -- leadtimes
     DATE_DIFF(confirmed_delivery_date, sales_document_date, DAY) AS leadtime_confirmed,
     DATE_DIFF(actual_goods_issue_date, sales_document_date, DAY) AS leadtime_agi,
     
-    -- CLASSIFICAÇÃO DE GAP
+    -- GAP classifications
     CASE 
         WHEN delivery_qty = 0 THEN "Sem entrega"
         WHEN delivery_qty < order_qty THEN "Parcial"
         ELSE "OK"
     END AS gap_category,
 
-    -- SEMANAS
+    -- Weeks
     FORMAT_DATE('%Y-%V', DATE(requested_delivery_date)) AS requested_week,
     FORMAT_DATE('%Y-%V', DATE(confirmed_delivery_date)) AS confirmed_week,
     FORMAT_DATE('%Y-%V', DATE(actual_goods_issue_date)) AS agi_week
